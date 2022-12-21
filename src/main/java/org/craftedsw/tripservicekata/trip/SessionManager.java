4ce346ed7e5889bd35bbb8dc5d33b9939258c4ea
Thomas Bénard <m.thomas.benard@gmail.com>
2022-12-21 10:38:31 +0100

    Extraction d'une interface

--- src/main/java/org/craftedsw/tripservicekata/trip/SessionManager.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/SessionManager.java
@@ -3,11 +3,12 @@ package org.craftedsw.tripservicekata.trip;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-public class SessionManager {
+public class SessionManager implements SessionManagerInterface {
     public SessionManager() {
     }
 
-    User getLoggedUser() {
+    @Override
+    public User getLoggedUser() {
         return UserSession.getInstance().getLoggedUser();
     }
 }
---
+++ src/main/java/org/craftedsw/tripservicekata/trip/SessionManagerInterface.java
@@ -0,0 +1,7 @@
+package org.craftedsw.tripservicekata.trip;
+
+import org.craftedsw.tripservicekata.user.User;
+
+public interface SessionManagerInterface {
+    User getLoggedUser();
+}
